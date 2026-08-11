dbcbad4ecfa5ad43aba641d34a22ea4c9b31c48c
Madhesh L <lmadhesh2006@gmail.com>
2026-08-11 11:37:46 +0530

    Create 2917-CountPairsWhoseSumIsLessThanTarget

---
+++ 2917-CountPairsWhoseSumIsLessThanTarget/2917-CountPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,16 @@
+// Last updated: 11/08/2026, 11:37:45
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        
+        int count =0;
+        for(int i=0; i<nums.size(); i++){
+            for (int j=i+1;j<nums.size();j++){
+                if(nums.get(i) + nums.get(j)< target){
+                    count++;
+                }
+            }
+
+        }return count;
+        
+    }
+}
